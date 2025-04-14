02868118f107dcd5609e72e31b09fa9606bc523d
AsmusRise <asris24@student.sdu.dk>
2025-04-14 13:48:57 +0200

    first commit

---
+++ Enemies.h
@@ -0,0 +1,16 @@
+using namespace std;
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+class Enemy
+{
+    public:
+    Enemy(string name, int health, int attackPower);
+
+    private:
+    string name;
+    
+
+};
