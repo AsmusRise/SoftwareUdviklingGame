2fcfdd3f75ba16dc56214958cb4434a6a8f1f703
AsmusRise <asris24@student.sdu.dk>
2025-06-02 12:12:18 +0200

    Vaersgo August

---
+++ build/CMakeFiles/TerminalGame.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Ape.cpp" "CMakeFiles/TerminalGame.dir/Ape.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Ape.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/DatabaseManager.cpp" "CMakeFiles/TerminalGame.dir/DatabaseManager.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/DatabaseManager.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Dragon.cpp" "CMakeFiles/TerminalGame.dir/Dragon.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Dragon.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Enemy.cpp" "CMakeFiles/TerminalGame.dir/Enemy.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Enemy.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/EnemyFactory.cpp" "CMakeFiles/TerminalGame.dir/EnemyFactory.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/EnemyFactory.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/GameManager.cpp" "CMakeFiles/TerminalGame.dir/GameManager.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/GameManager.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Goblin.cpp" "CMakeFiles/TerminalGame.dir/Goblin.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Goblin.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Grotte.cpp" "CMakeFiles/TerminalGame.dir/Grotte.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Grotte.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Hest.cpp" "CMakeFiles/TerminalGame.dir/Hest.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Hest.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/MainCharacter.cpp" "CMakeFiles/TerminalGame.dir/MainCharacter.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/MainCharacter.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Snake.cpp" "CMakeFiles/TerminalGame.dir/Snake.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Snake.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Source.cpp" "CMakeFiles/TerminalGame.dir/Source.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Source.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Unicorn.cpp" "CMakeFiles/TerminalGame.dir/Unicorn.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Unicorn.cpp.o.d"
+  "/home/asmusrise/Documents/softwareUdvikling/Game/Weapons.cpp" "CMakeFiles/TerminalGame.dir/Weapons.cpp.o" "gcc" "CMakeFiles/TerminalGame.dir/Weapons.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TerminalGame.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TerminalGame.dir/Ape.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Ape.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/DatabaseManager.cpp.o"
+  "CMakeFiles/TerminalGame.dir/DatabaseManager.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Dragon.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Dragon.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Enemy.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Enemy.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/EnemyFactory.cpp.o"
+  "CMakeFiles/TerminalGame.dir/EnemyFactory.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/GameManager.cpp.o"
+  "CMakeFiles/TerminalGame.dir/GameManager.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Goblin.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Goblin.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Grotte.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Grotte.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Hest.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Hest.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/MainCharacter.cpp.o"
+  "CMakeFiles/TerminalGame.dir/MainCharacter.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Snake.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Snake.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Source.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Source.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Unicorn.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Unicorn.cpp.o.d"
+  "CMakeFiles/TerminalGame.dir/Weapons.cpp.o"
+  "CMakeFiles/TerminalGame.dir/Weapons.cpp.o.d"
+  "TerminalGame"
+  "TerminalGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TerminalGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
